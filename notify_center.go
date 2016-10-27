1a8668e3762b104088b0168065cfc0edb4439f1f
Maxime Devalland <maxime@devalland.com>
2016-10-27 10:47:07 -0700

    Update notify_center.go

--- notify_center.go
+++ notify_center.go
@@ -12,8 +12,8 @@ package main
 
 import (
 	log "github.com/cihub/seelog"
-	"github.com/linkedin/Burrow/notifier"
-	"github.com/linkedin/Burrow/protocol"
+	"github.com/opentable/Burrow/notifier"
+	"github.com/opentable/Burrow/protocol"
 	"math/rand"
 	"net"
 	"net/http"
